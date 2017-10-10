b93a5228b49325fcd059d058084698b5231d4603
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-10-10 23:44:21 +0100

    Add basic code for bot

---
+++ app.js
@@ -0,0 +1,12 @@
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+const bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+const rtm = new RtmClient(bot_token);
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+rtm.start();
