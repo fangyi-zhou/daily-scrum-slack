3959acd3fbf6a0f60cc5dbc9d375ac09c83a68ca
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-10-11 01:05:02 +0100

    Get name of message sender

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const RtmClient = require('@slack/client').RtmClient;
+const WebClient = require('@slack/client').WebClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 const bot_token = process.env.SLACK_BOT_TOKEN || '';
+const web_token = process.env.SLACK_API_TOKEN || '';
 
 const rtm = new RtmClient(bot_token);
+const web = new WebClient(web_token);
+
+let uidToName = {};
+let nameToUid = {};
+let userToDM = {};
+let DMToUser = {};
+
+web.users.list((err, info) => {
+    if (err) {
+        console.log(err);
+    } else {
+        for (const user of info.members) {
+            uidToName[user.id] = user.name;
+            nameToUid[user.name] = user.id;
+        }
+    }
+});
+
+web.im.list((err, info) => {
+    if (err) {
+        console.log(err);
+    } else {
+        for (const im of info.ims) {
+            userToDM[im.user] = im.id;
+            DMToUser[im.id] = im.user;
+        }
+    }
+});
 
 let scrumChannel;
 
@@ -24,7 +54,7 @@ rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
         rtm.sendMessage("TODO: Send digest", scrumChannel);
     }
     if (msg.channel.startsWith("D")) {
-        rtm.sendMessage(msg.text, msg.channel);
+        rtm.sendMessage(msg.text + " From " +uidToName[DMToUser[msg.channel]], msg.channel);
     }
 });
 
